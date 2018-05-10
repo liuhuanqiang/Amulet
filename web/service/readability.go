295d75125361e06af8cdfe0a8710b3a53bf39419
iloster <243849707@qq.com>
2018-05-10 23:17:37 +0800

    去掉了只有一个子标签的div

--- web/service/readability.go
+++ web/service/readability.go
@@ -62,27 +62,48 @@ func (this *Readability) GetContent(url string) {
 		}
 		node = this.getNextNode(node, false)
 	}
-
+	fmt.Println("html:", this.renderNode(bodyNode))
 	node = bodyNode.FirstChild
 	for node != nil {
 		// 第三步 如果标签div中只有一个children, 则去掉这个div。 如果只有文字，就用p替换
 		if node.DataAtom == atom.Div {
 			if this.hasSinglePInsideElement(node) {
 				// 如果里面只有一个p, 则去掉这个div
-				newNode := this.getNodeChildrens(node)[0]
-				node.Parent.RemoveChild(node)
+				fmt.Println("[p]", node.Attr)
+				child := this.getNodeChildrens(node)[0]
+				newNode := &html.Node {
+					Type: child.Type,
+					DataAtom:child.DataAtom,
+					Data: child.Data,
+					Namespace:child.Namespace,
+					Attr:child.Attr,
+					FirstChild: child.FirstChild,
+					LastChild:nil,
+				}
 				node.Parent.AppendChild(newNode)
-			} else if this.hasChildBlockElement(node) {
-				newNode := this.getNodeChildrens(node)[0]
-				fmt.Println("[block]:")
-				node.Parent.InsertBefore(newNode, node)
 				node.Parent.RemoveChild(node)
-
+				node = this.getNextNode(newNode.Parent,false)
+			} else if this.hasChildBlockElement(node) {
+				fmt.Println("[block]", node.Attr)
+				child := this.getNodeChildrens(node)[0]
+				newNode := &html.Node {
+					Type: child.Type,
+					DataAtom:child.DataAtom,
+					Data: child.Data,
+					Namespace:child.Namespace,
+					Attr:child.Attr,
+					FirstChild: child.FirstChild,
+					LastChild:nil,
+				}
+				node.Parent.AppendChild(newNode)
+				node.Parent.RemoveChild(node)
+				node = this.getNextNode(newNode.Parent,false)
+				continue
 			}
 		}
 		node = this.getNextNode(node, false)
 	}
-	fmt.Println("html:", this.renderNode(bodyNode))
+	fmt.Println("[html]:", this.renderNode(bodyNode))
 }
 
 func (this *Readability) getHtml(url string) string {
@@ -91,6 +112,7 @@ func (this *Readability) getHtml(url string) string {
 	req.Header.Set("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
 	resp, _ := client.Do(req)
 	str,_ := ioutil.ReadAll(resp.Body)
+	defer  resp.Body.Close()
 	return string(str)
 }
 
@@ -108,17 +130,19 @@ func (this *Readability) getBody(str string) string {
 	body = reg_style.ReplaceAllString(body,"")
 
 	// 过滤注释
-	reg_anno := regexp.MustCompile(`(?sU:<!--).*?(?s:-->)`)
+	reg_anno := regexp.MustCompile(`(?sU:<!).*?(?s:>)`)
 	body = reg_anno.ReplaceAllString(body,"")
 
 	return body
 }
 
-func (this *Readability)renderNode(n *html.Node) string {
+func (this *Readability) renderNode(n *html.Node) string {
 	var buf bytes.Buffer
 	w := io.Writer(&buf)
 	html.Render(w, n)
-	return buf.String()
+	reg := regexp.MustCompile(`(?sU:</*html.*>)|(?sU:</*head.*>)|(?sU:</*body.*>)`)
+	return reg.ReplaceAllString(buf.String(), "")
+	//return buf.String()
 }
 
 func (this *Readability) getNodeContent(n *html.Node) string {
